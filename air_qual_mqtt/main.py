73b876cd07358e92558a9d0463e431fdbd346d05
Arvont Hill <arhill@borgwarner.com>
2023-06-12 16:31:05 -0400

    add basic functionality to air_qual_mqtt

--- air_qual_mqtt/main.py
+++ air_qual_mqtt/main.py
@@ -16,6 +16,9 @@ from secrets import ap, pw
 #Constants
 WIDTH  = const(128)         # oled display width
 HEIGHT = const(64)          # oled display height
+BUFFER_WIDTH = const(4)
+CHARACTER_WIDTH = const(8)
+CHARACTER_HEIGHT = const(8)
 
 #Strings
 aqi_str =  '   AQI: '
@@ -26,8 +29,8 @@ stat_str = 'Status: '
 #i2c variables
 i2c0_scl = Pin(5)
 i2c0_sda = Pin(4)
-i2c0_freq = 400000
-i2c0_bus = 0
+i2c0_freq = const(400000)
+i2c0_bus = const(0)
 
 #wlan variables
 
@@ -38,8 +41,11 @@ aqi_topic = b'home-assistant/livingroom/aqi'
 eco2_topic = b'home-assistant/livingroom/eco2'
 tvoc_topic = b'home-assistant/livingroom/tvoc'
 
-
 conversion_factor = 3.3 / (65535)
+#Create a framebuffer that can be used to clear text
+s = BUFFER_WIDTH * 8 * [0]
+blank_buffer = bytearray(s)
+blank_fb = framebuf.FrameBuffer(blank_buffer, BUFFER_WIDTH * CHARACTER_WIDTH, CHARACTER_WIDTH, framebuf.MONO_HLSB)
 
 def init_system():
     """setup i2c devices, make the wifi connection, connect to mqtt broker"""
@@ -78,15 +84,6 @@ def init_system():
 
     return oled, sensor
 
-def create_topic_dict(sensor):
-    """given a sensor object create a dictionary of where the topic to publish is the key
-    and the sensor reading is the value"""
-    sensor_dict = {}
-    sensor_dict[b'home-assistant/livingroom/aqi'] = sensor.aqi
-    sensor_dict[b'home-assistant/livingroom/eco2'] = sensor.eco2
-    sensor_dict[b'home-assistant/livingroom/tvoc'] = sensor.tvoc
-    return sensor_dict
-
 def mqtt_connect():
     client = MQTTClient(client_id, mqtt_server, keepalive=3600)
     client.connect()
@@ -98,19 +95,74 @@ def reconnect():
     time.sleep(5)
     machine.reset()
 
-def display_sensor_data():
+def display_sensor_data(oled,aqi,eco2,tvoc,operation):
     """display sensor data on the oled"""
+    meas_count = 0
+    sum_readings = 0
+    for i in range(5):
+        sum_readings+=sensor_temp.read_u16() * conversion_factor
+        meas_count+=1
+        utime.sleep(0.010)
+        
+    oled.show()
+    reading = sum_readings / meas_count
+    celsius_degrees = 27 - (reading - 0.706)/0.001721
+    fahrenheit_degrees = celsius_degrees * 9 / 5 + 32
+    
+    # Print air quality metrics and temperature
+    print(aqi_str + str(aqi.value) + ' [' + str(aqi.rating) +']')
+    print(tvoc_str + str(tvoc) + ' ppb')
+    print(eco2_str + str(eco2.value) + ' ppm [' + str(eco2.rating) +']')
+    print(stat_str + operation)
+    print('  Temp: ' +str(round(fahrenheit_degrees,1)) + '°F')
+    print('--------------------------------')
+    text_y = 8
+    oled.blit(blank_fb,0,text_y)
+    oled.blit(blank_fb,BUFFER_WIDTH*8,text_y)
+    oled.text(sensor.operation,0,8)
+    text_y = 20
+    oled.blit(blank_fb,65,text_y)
+    oled.text(str(aqi.value),65,text_y)
+    text_y = 30
+    oled.blit(blank_fb,65,text_y)
+    oled.text(str(tvoc),65,text_y)
+    text_y = 40
+    oled.blit(blank_fb,65,text_y)
+    oled.text(str(eco2.value),65,text_y)
+
+    text_y = 50
+    oled.blit(blank_fb,65,text_y)
+    oled.text(str(round(fahrenheit_degrees,1)),65,50)
+    utime.sleep(5)
+
+def display_fatal_error():
+    """Display an error if the sensor or network doesn't work"""
     pass
 
-def publish_mqtt_data(sensor_dict):
-    """publish mqtt data"""
+def main():
+    """Main Loop"""
+    oled, sensor = init_system()
     try:
-        client = mqtt_connect()
+        mqtt_client = mqtt_connect()
     except OSError as e:
         reconnect()
-    for topic in sensor_dict:
-        client.publish(topic,sensor_dict[topic])
-    client.disconnect()
+
+    sensor_dict = {}
+
+    while True:
+        # Read from the sensor
+        aqi_value = sensor.aqi.value
+        tvoc_value = sensor.tvoc.value
+        eco2_value = sensor.eco2.value
+        operation_value = sensor.operation.value
+
+        #publish the data
+        mqtt_client.publish(aqi_topic,aqi_value)
+        mqtt_client.publish(tvoc_topic,tvoc_value)
+        mqtt_client.publish(eco2_topic,eco2_value)
+
+        #display sensor data
+        display_sensor_data(aqi_value,eco2_value,tvoc_value,operation_value)
 
 
 
